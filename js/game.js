4efa31d74a6480924c134bf1e31866ea4a25126d
Abayomi-Silva <abayomi.lukman30@gmail.com>
2023-09-09 02:07:02 -0300

    Unidades 09 a 12

---
+++ js/game.js
@@ -0,0 +1,61 @@
+ // JavaScript (pode ser incluído inline ou em um arquivo externo)
+ let currentPlayer = "X";
+ let playerXName = "";
+ let playerOName = "";
+ let gameOver = false;
+
+ function startGame() {
+     playerXName = document.getElementById("playerX").value || "Jogador X";
+     playerOName = document.getElementById("playerO").value || "Jogador O";
+
+     if (playerXName && playerOName) {
+         document.querySelector("div:first-of-type").style.display = "none";
+         document.querySelector("div:last-of-type").style.display = "block";
+         updateStatus();
+     }
+ }
+
+ function updateStatus() {
+     document.getElementById("currentPlayerDisplay").textContent = currentPlayer === "X" ? playerXName : playerOName;
+     document.querySelector(".status").textContent = `Vez de ${document.getElementById("currentPlayerDisplay").textContent}`;
+ }
+
+ function makeMove(cell) {
+     if (!cell.textContent && !gameOver) {
+         cell.textContent = currentPlayer;
+         checkWinner();
+         currentPlayer = currentPlayer === "X" ? "O" : "X";
+         updateStatus();
+     }
+ }
+
+ function checkWinner() {
+     const board = document.querySelectorAll(".cell");
+     const winningCombos = [
+         [0, 1, 2], [3, 4, 5], [6, 7, 8], // Linhas
+         [0, 3, 6], [1, 4, 7], [2, 5, 8], // Colunas
+         [0, 4, 8], [2, 4, 6]             // Diagonais
+     ];
+
+     for (const combo of winningCombos) {
+         const [a, b, c] = combo;
+         const cellA = board[a].textContent;
+         const cellB = board[b].textContent;
+         const cellC = board[c].textContent;
+
+         if (cellA && cellA === cellB && cellA === cellC) {
+             gameOver = true;
+             const winner = cellA === "X" ? playerXName : playerOName;
+             document.querySelector(".status").textContent = `${winner} venceu!`;
+             alert(`${winner} venceu!`);
+             return;
+         }
+     }
+
+     // Verifica se há empate
+     if ([...board].every(cell => cell.textContent !== "") && !gameOver) {
+         gameOver = true;
+         document.querySelector(".status").textContent = "Empate!";
+         alert("O jogo empatou!");
+     }
+ }
--- js/script.js
+++ js/script.js
@@ -1,42 +1,18 @@
-function displayName1(){
-    var imagem1 = document.getElementById("lua")
-    if (imagem1.src.match("./images/lua.jpg")){
-        document.getElementById("moon").innerHTML="Lua"
-    }
-    else{
-        document.getElementById("moon").innerHTML="Sol"
-    }   
-}
-function displayName2(){
-    var imagem2 = document.getElementById("sol")
-    if (imagem2.src.match("./images/sol.jpg")){
-        document.getElementById("sun").innerHTML="Sol"
-    }
-    else{
-        document.getElementById("sun").innerHTML="Lua"
-    }
-}
-function trocarImagem1() {
-    var imagem = document.getElementById("lua");
-    // Verifica qual imagem está sendo exibida
-    if (imagem.src.match("images/lua.jpg")) {
-    // Se for a primeira imagem, troque para a segunda imagem
-    imagem.src = "./images/sol.jpg";
+function swap(id) 
+{
+    if (document.getElementById(id).getAttribute("src") == "images/sol.jpg") {
+        document.getElementById(id).setAttribute("src","images/lua.jpg");
     } 
     else {
-    // Se for a segunda imagem, troque de volta para a primeira imagem
-    imagem.src = "./images/lua.jpg";
+        document.getElementById(id).setAttribute("src","images/sol.jpg");
     }
 }
-function trocarImagem2() {
-    var imagem = document.getElementById("sol");
-    // Verifica qual imagem está sendo exibida
-    if (imagem.src.match("images/sol.jpg")) {
-    // Se for a primeira imagem, troque para a segunda imagem
-        imagem.src = "./images/lua.jpg";
+
+function show(id){
+    if (document.getElementById(id).getAttribute("src") == "images/sol.jpg") {
+        document.getElementById("show").innerHTML = "Sol"
     } 
     else {
-    // Se for a segunda imagem, troque de volta para a primeira imagem
-        imagem.src = "./images/sol.jpg";
+        document.getElementById("show").innerHTML = "Lua"
     }
 }
