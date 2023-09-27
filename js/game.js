c61a8089beb51f3961ee2c572efcc8693e75fd9a
Abayomi-Silva <143351852+Abayomi-Silva@users.noreply.github.com>
2023-09-27 01:03:23 -0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -11,20 +11,26 @@ function comecar() {
         vezJogar();
     }
 }
+
 function vezJogar() {
-    document.getElementById("vezJogador").textContent = jogador === "X" ? jogadorX : jogadorO;
-    document.getElementById(".status").textContent = `Vez de ${document.getElementById("vezJogador").textContent}`;
+    const vezJogadorElement = document.getElementById("vezJogador");
+    const statusElement = document.querySelector(".status");
+    
+    if (vezJogadorElement && statusElement) {
+        vezJogadorElement.textContent = jogador === "X" ? jogadorX : jogadorO;
+        
+    }
 }
-function jogar(cell) {
-    if (!cell.textContent && !gameOver) {
-        cell.textContent = jogador;
+function jogar(blocos) {
+    if (!blocos.textContent && !gameOver) {
+        blocos.textContent = jogador;
         vencedor();
         jogador = jogador === "X" ? "O" : "X";
         vezJogar();
     }
 }
 function vencedor() {
-    const board = document.querySelectorAll(".cell");
+    const board = document.querySelectorAll(".blocos");
     const winningCombos = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8], 
         [0, 3, 6], [1, 4, 7], [2, 5, 8], 
@@ -32,18 +38,18 @@ function vencedor() {
         ];
     for (const combo of winningCombos) {
         const [a, b, c] = combo;
-        const cellA = board[a].textContent;
-        const cellB = board[b].textContent;
-        const cellC = board[c].textContent;
-        if (cellA && cellA === cellB && cellA === cellC) {
+        const blocosA = board[a].textContent;
+        const blocosB = board[b].textContent;
+        const blocosC = board[c].textContent;
+        if (blocosA && blocosA === blocosB && blocosA === blocosC) {
             gameOver = true;
-            const winner = cellA === "X" ? jogadorX : jogadorO;
+            const winner = blocosA === "X" ? jogadorX : jogadorO;
             document.querySelector(".status").textContent = `${winner} venceu!`;
             alert(`${winner} venceu!`);
             return;
         }
     }
-    if ([...board].every(cell => cell.textContent !== "") && !gameOver) {
+    if ([...board].every(blocos => blocos.textContent !== "") && !gameOver) {
         gameOver = true;
         document.querySelector(".status").textContent = "Empate!";
         alert("O jogo empatou!");
