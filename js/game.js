8beab32ddb92cde1ab108c06dd521b4d767bee28
Abayomi-Silva <abayomi.lukman30@gmail.com>
2023-09-26 23:52:43 -0300

    Unidades 09 a 12

--- js/game.js
+++ js/game.js
@@ -1,61 +1,60 @@
- // JavaScript (pode ser incluído inline ou em um arquivo externo)
- let currentPlayer = "X";
- let playerXName = "";
- let playerOName = "";
- let gameOver = false;
+let jogador = "X";
+let jogadorX = "";
+let jogadorO = "";
+let gameOver = false;
 
- function startGame() {
-     playerXName = document.getElementById("playerX").value || "Jogador X";
-     playerOName = document.getElementById("playerO").value || "Jogador O";
+function comecar() {
+    jogadorX = document.getElementById("playerX").value || "Jogador X";
+    jogadorO = document.getElementById("playerO").value || "Jogador O";
+    if (jogadorX && jogadorO) {
+        document.querySelector("div:first-of-type").style.display = "none";
+        document.querySelector("div:last-of-type").style.display = "block";
+        vezJogar();
+        
+    }
+}
+function vezJogar() {
+  
+    const vezJogadorElement = document.getElementById("vezJogador");
+    const statusElement = document.querySelector(".status");
+    
+    if (vezJogadorElement && statusElement) {
+        vezJogadorElement.textContent = jogador === "X" ? jogadorX : jogadorO;
+        statusElement.textContent = `Vez de ${vezJogadorElement.textContent}`;
+    }
+}
 
-     if (playerXName && playerOName) {
-         document.querySelector("div:first-of-type").style.display = "none";
-         document.querySelector("div:last-of-type").style.display = "block";
-         updateStatus();
-     }
- }
-
- function updateStatus() {
-     document.getElementById("currentPlayerDisplay").textContent = currentPlayer === "X" ? playerXName : playerOName;
-     document.querySelector(".status").textContent = `Vez de ${document.getElementById("currentPlayerDisplay").textContent}`;
- }
-
- function makeMove(cell) {
-     if (!cell.textContent && !gameOver) {
-         cell.textContent = currentPlayer;
-         checkWinner();
-         currentPlayer = currentPlayer === "X" ? "O" : "X";
-         updateStatus();
-     }
- }
-
- function checkWinner() {
-     const board = document.querySelectorAll(".cell");
-     const winningCombos = [
-         [0, 1, 2], [3, 4, 5], [6, 7, 8], // Linhas
-         [0, 3, 6], [1, 4, 7], [2, 5, 8], // Colunas
-         [0, 4, 8], [2, 4, 6]             // Diagonais
-     ];
-
-     for (const combo of winningCombos) {
-         const [a, b, c] = combo;
-         const cellA = board[a].textContent;
-         const cellB = board[b].textContent;
-         const cellC = board[c].textContent;
-
-         if (cellA && cellA === cellB && cellA === cellC) {
-             gameOver = true;
-             const winner = cellA === "X" ? playerXName : playerOName;
-             document.querySelector(".status").textContent = `${winner} venceu!`;
-             alert(`${winner} venceu!`);
-             return;
-         }
-     }
-
-     // Verifica se há empate
-     if ([...board].every(cell => cell.textContent !== "") && !gameOver) {
-         gameOver = true;
-         document.querySelector(".status").textContent = "Empate!";
-         alert("O jogo empatou!");
-     }
- }
+function jogar(blocos) {
+    if (!blocos.textContent && !gameOver) {
+        blocos.textContent = jogador;
+        vencedor();
+        jogador = jogador === "X" ? "O" : "X";
+        vezJogar();
+    }
+}
+function vencedor() {
+    const board = document.querySelectorAll(".blocos");
+    const winningCombos = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+        [0, 4, 8], [2, 4, 6]            
+        ];
+    for (const combo of winningCombos) {
+        const [a, b, c] = combo;
+        const blocosA = board[a].textContent;
+        const blocosB = board[b].textContent;
+        const blocosC = board[c].textContent;
+        if (blocosA && blocosA === blocosB && blocosA === blocosC) {
+            gameOver = true;
+            const winner = blocosA === "X" ? jogadorX : jogadorO;
+            document.querySelector(".status").textContent = `${winner} venceu!`;
+            alert(`${winner} venceu!`);
+            return;
+        }
+    }
+    if ([...board].every(blocos => blocos.textContent !== "") && !gameOver) {
+        gameOver = true;
+        document.querySelector(".status").textContent = "Empate!";
+        alert("O jogo empatou!");
+    }
+}
